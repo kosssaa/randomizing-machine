9ad0eeeda5a6d335d0bac3ea7d8d9d08379c4e6d
kosssaa <kosssaa@o2.pl>
2019-10-22 18:41:22 +0200

    1st step

---
+++ src/main/java/com/j25/cloud/randomizingmachine/RandomizingMachineApplication.java
@@ -0,0 +1,13 @@
+package com.j25.cloud.randomizingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomizingMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomizingMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/j25/cloud/randomizingmachine/RandomizingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j25.cloud.randomizingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomizingMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
