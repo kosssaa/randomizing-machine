c041c2f9d87b951858d6309d8ffeebb9fb2d29c2
kosssaa <kosssaa@o2.pl>
2019-10-22 18:57:18 +0200

    2nd step

--- src/main/java/com/j25/cloud/randomizingmachine/RandomizingMachineApplication.java
+++ src/main/java/com/j25/cloud/randomizingmachine/RandomizingMachineApplication.java
@@ -2,7 +2,9 @@ package com.j25.cloud.randomizingmachine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RandomizingMachineApplication {
 
---
+++ src/main/java/com/j25/cloud/randomizingmachine/controller/RandomizingController.java
@@ -0,0 +1,26 @@
+package com.j25.cloud.randomizingmachine.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class RandomizingController {
+    @Value("${random.words})")
+    private String[] words;
+
+    @Value("${random.numbers})")
+    private String[] numbers;
+
+    @GetMapping("/word")
+    public String randomWord(){
+        return words[new Random().nextInt(words.length)];
+    }
+
+    @GetMapping("/number")
+    public String randomNumber(){
+        return numbers[new Random().nextInt(numbers.length)];
+    }
+}
